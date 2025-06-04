e90ec13c2eb99634051b3a288eea9f7e4f697495
extramohsin <swaggymmgemss@gmail.com>
2025-06-05 03:01:32 +0530

    Update config.js

--- genwin-client/src/config.js
+++ genwin-client/src/config.js
@@ -1,6 +1,3 @@
-const API_URL =
-  import.meta.env.NODE_ENV === "production"
-    ? "https://your-backend-url.onrender.com"
-    : "http://localhost:5000";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 export default API_URL;
