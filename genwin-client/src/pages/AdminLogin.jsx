b86f8ce7ddfa119eeb15a08dab15ddb70ced2e2b
extramohsin <swaggymmgemss@gmail.com>
2025-06-05 04:15:13 +0530

    Update AdminLogin.jsx

--- genwin-client/src/pages/AdminLogin.jsx
+++ genwin-client/src/pages/AdminLogin.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import API_URL from "../config";
 
 const AdminLogin = () => {
   const navigate = useNavigate();
@@ -14,7 +15,7 @@ const AdminLogin = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/api/admin/login", {
+      const response = await fetch(`${API_URL}/api/admin/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
